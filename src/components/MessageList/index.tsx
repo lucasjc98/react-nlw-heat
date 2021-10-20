26cf6e80782d3fd99042a15630355600cd34dafa
Lucas Costa <lucasjcosta.98@gmail.com>
2021-10-20 12:01:41 -0300

    Add messages from BD in component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,56 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
 
 import logoImg from "../../assets/logo.svg";
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export function MessageList() {
-  useEffect(() => {}, [])
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
 
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="Do While 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
-            commodi dolore voluptate expedita, doloremque ipsam.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/lucasjc98.png" alt="Lucas Costa" />
-            </div>
-            <span>Lucas Costa</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
-            commodi dolore voluptate expedita, doloremque ipsam.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/lucasjc98.png" alt="Lucas Costa" />
-            </div>
-            <span>Lucas Costa</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
-            commodi dolore voluptate expedita, doloremque ipsam.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/lucasjc98.png" alt="Lucas Costa" />
-            </div>
-            <span>Lucas Costa</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={message.user.avatar_url}
+                    alt={message.user.avatar_url}
+                  />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
