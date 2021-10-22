4f0f51b6864830396c3834738f90b77ceed712e6
Lucas Costa <lucasjcosta.98@gmail.com>
2021-10-22 17:09:50 -0300

    Add minor fix

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -33,7 +33,7 @@ type AuthResponse = {
 export function AuthProvider(props: AuthProvider) {
   const [user, setUser] = useState<User | null>(null);
 
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=`;
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=YOUR_CLIENT_ID`;
 
   async function signIn(githubCode: string) {
     const response = await api.post<AuthResponse>("authenticate", {
